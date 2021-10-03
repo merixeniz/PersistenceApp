ee9ab8d8f2d80c309903effd8864ed89d130fd1a
Szymon Oles <szyoles@gmail.com>
2021-10-03 14:41:00 +0200

    Test project added + Algorytmy notes and SQL Notes

---
+++ PersistenceApp/Persistence.Tests/Tests.cs
@@ -0,0 +1,21 @@
+using Application.Algorithms.Recursion;
+using FluentAssertions;
+using Xunit;
+
+namespace Persistence.Tests;
+public class Tests
+{
+    [Fact]
+    public void Fibonacci()
+    {
+        var fibo = new Fibonacci();
+
+        fibo.FibonacciNumber(0).Should().Be(0);
+        fibo.FibonacciNumber(1).Should().Be(1);
+        fibo.FibonacciNumber(2).Should().Be(1);
+        fibo.FibonacciNumber(5).Should().Be(5);
+        fibo.FibonacciNumber(6).Should().Be(8);
+        fibo.FibonacciNumber(7).Should().Be(13);
+        fibo.FibonacciNumber(8).Should().Be(21);
+    }
+}
