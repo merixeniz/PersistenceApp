5620a6f0b54a01d8c29ed189e18a5b3bc3dd8c53
Szymon Oleś <s.oles@soniqsoft.pl>
2021-10-11 13:52:49 +0200

    add: binary search

---
+++ PersistenceApp/Application/Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Algorithms.Searching
+{
+    public class BinarySearch
+    {
+        // input has to be sorted!
+        public int Search(List<int> input, int target)
+        {
+            var midPointer = (input.Count - 1) / 2;
+
+            while (input[midPointer] != target)
+            {
+                if (input[midPointer] > target)
+                    midPointer--;
+
+                if (input[midPointer] < target)
+                    midPointer++;
+            }
+
+            return midPointer;
+        }
+    }
+}
---
+++ PersistenceApp/Persistence.Tests/Searching.cs
@@ -0,0 +1,26 @@
+﻿using Application.Algorithms.Searching;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Persistence.Tests
+{
+    public class Searching
+    {
+
+        [Fact]
+        public void BinarySearch()
+        {
+            var sut = new BinarySearch();
+            var input = new List<int> { -5, -4, -3, -2, 0, 1, 2, 3, 4, 5 };
+
+            sut.Search(input, 2).Should().Be(6);
+            sut.Search(input, -5).Should().Be(0);
+            sut.Search(input, 0).Should().Be(4);
+        }
+    }
+}
