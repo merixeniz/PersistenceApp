a0530f5901aea98367488b3c1b952f363da18b05
DESKTOP-L113IGH\Szymon <szyoles@gmail.com>
2023-11-18 22:56:32 +0100

    string problems

---
+++ PersistenceApp/Application/Algorithms/TakeYouForward/Strings.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Algorithms.TakeYouForward
+{
+    public static partial class TakeUForward
+    {
+        public static class Strings
+        {
+            // https://leetcode.com/problems/sort-characters-by-frequency/
+            public static string FrequencySort(string s)
+            {
+                var concatenatedValues = string.Empty;
+
+                //var groupedAndOrdered = s
+                //    .GroupBy(x => x)
+                //    .OrderByDescending(x => x.Count());
+
+                //foreach (var group in groupedAndOrdered)
+                //{
+                //    foreach (var value in group)
+                //    {
+                //        concatenatedValues += value;
+                //    }
+                //}
+
+                concatenatedValues = string.Concat(s
+                        .GroupBy(@char => @char)
+                        .OrderByDescending(@char => @char.Count())
+                        .SelectMany(groupItem => groupItem)
+                );
+
+                return concatenatedValues;
+            }
+
+            //Maximum Nesting Depth of the Parentheses
+            public static int MaxDepth(string s)
+            {
+                if (string.IsNullOrEmpty(s)) return 0;
+
+                var results = new HashSet<int>();
+                int openBracketCount = 0;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(') openBracketCount++;
+                    if (s[i] == ')') openBracketCount--;
+                    results.Add(openBracketCount);
+                }
+
+                return results.Max();
+            }
+
+            public static int MaxDepthAggregate(string s)
+            {
+                if (string.IsNullOrEmpty(s)) return 0;
+                var results = new HashSet<int>();
+
+                s.Aggregate(0, (acc, currentChar) =>
+                {
+                    if (currentChar == '(') acc++;
+                    if (currentChar == ')') acc--;
+                    results.Add(acc);
+                    return acc;
+                });
+                return results.Max();
+            }
+
+            public static int RomanToInt(string s)
+            {
+                if (string.IsNullOrEmpty(s)) return 0;
+                if (s.Length > 15) return -1;
+
+                var romanNumbersDict = new Dictionary<string, int>()
+                {
+                    {"I", 1},
+                    {"V", 5},
+                    {"X", 10},
+                    {"L", 50},
+                    {"C", 100},
+                    {"D", 500},
+                    {"M", 1000},
+                    {"IV", 4},
+                    {"IX", 9},
+                    {"XL", 40},
+                    {"XC", 90},
+                    {"CD", 400},
+                    {"CM", 900},
+                };
+
+                int result = 0;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if ((s[i] == 'I' || s[i] == 'X' || s[i] == 'C') && i < s.Length - 1)
+                    {
+                        var tmp = string.Concat("", s[i], s[i + 1]);
+                        if (romanNumbersDict.ContainsKey(tmp))
+                        {
+                            result += romanNumbersDict[tmp];
+                            i++;
+                            continue;
+                        }
+                    }
+
+                    result += romanNumbersDict[s[i].ToString()];
+                }
+
+                return result;
+            }
+        }
+    }
+
+
+}
---
+++ PersistenceApp/Persistence.Tests/StringsTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Algorithms.TakeYouForward;
+using Application.Algorithms.Trees;
+using FluentAssertions;
+using Xunit;
+
+namespace Persistence.Tests
+{
+    public class StringsTests
+    {
+        [Theory]
+        [InlineData("hello", "llheo")] // General case
+        [InlineData("abracadabra", "aaaaabbrrcd")] // Case with repeated characters
+        [InlineData("", "")] // Empty string
+        [InlineData("a", "a")] // String with a single character
+        [InlineData("xyyz", "yyxz")] // Case with repeated characters and sorting
+        [InlineData("AaAAaA", "AAAAaa")] // Case with mixed case characters
+        public void FrequencySortTest(string input, string result)
+        {
+            TakeUForward.Strings.FrequencySort(input).Should().Be(result);
+        }
+
+        [Theory]
+        [InlineData("()()()()()", 1)] // Same level, minimal depth
+        [InlineData("(1+2)", 1)] // Single pair
+        [InlineData("((()))", 3)] // Nested to the max
+        [InlineData("((1+2)*(3-4))", 2)] // Mix of arithmetic and nesting
+        [InlineData("(1+(2*3)+((8)/4))+1", 3)] // Example from the original test
+        [InlineData("(((()))())", 4)] // Deep nesting with some extra characters
+        [InlineData("()()(()(()))", 3)] // Balanced mix of nesting and non-nesting
+        [InlineData("()", 1)] // Smallest valid input
+        [InlineData("", 0)] // Empty input
+        public void MaxDepthParenthesisTest(string input, int expected)
+        {
+            TakeUForward.Strings.MaxDepth(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("()()()()()", 1)] // Same level, minimal depth
+        [InlineData("(1+2)", 1)] // Single pair
+        [InlineData("((()))", 3)] // Nested to the max
+        [InlineData("((1+2)*(3-4))", 2)] // Mix of arithmetic and nesting
+        [InlineData("(1+(2*3)+((8)/4))+1", 3)] // Example from the original test
+        [InlineData("(((()))())", 4)] // Deep nesting with some extra characters
+        [InlineData("()()(()(()))", 3)] // Balanced mix of nesting and non-nesting
+        [InlineData("()", 1)] // Smallest valid input
+        [InlineData("", 0)] // Empty input
+        public void MaxDepthParenthesisAggregateTest(string input, int expected)
+        {
+            TakeUForward.Strings.MaxDepthAggregate(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        [InlineData("IX", 9)] // Basic subtraction case
+        [InlineData("XC", 90)] // Another subtraction case
+        [InlineData("XL", 40)] // Yet another subtraction case
+        [InlineData("CD", 400)] // Subtraction with D
+        [InlineData("CM", 900)] // Subtraction with M
+        [InlineData("VII", 7)] // Basic addition case
+        [InlineData("XII", 12)] // Another addition case
+        [InlineData("XXI", 21)] // Yet another addition case
+        [InlineData("CXLVII", 147)] // Complex case
+        [InlineData("MMXXI", 2021)] // Current year
+        [InlineData("MMMCMXC", 3990)] // Maximum representation up to 3999
+        [InlineData("", 0)] // Empty string
+        [InlineData("MMMMCMXCIX", 4999)] // Maximum representation with 4999
+        public void RomanToInt(string input, int result)
+        {
+            TakeUForward.Strings.RomanToInt(input).Should().Be(result);
+        }
+    }
+}
