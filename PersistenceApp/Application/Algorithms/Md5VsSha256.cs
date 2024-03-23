22f1df7079a177c3a1219ef93b9a31b5022c76c7
DESKTOP-L113IGH\Szymon <szyoles@gmail.com>
2024-03-24 00:15:19 +0100

    up to speech services

--- PersistenceApp/Application/Algorithms/Md5VsSha256.cs
+++ PersistenceApp/Application/Algorithms/Md5VsSha256.cs
@@ -6,19 +6,19 @@ namespace Application.Algorithms
     public class Md5VsSha256
     {
         private const int N = 10000;
-        private readonly byte[] data;
+        private readonly byte[] _data;
 
-        private readonly SHA256 sha256 = SHA256.Create();
-        private readonly MD5 md5 = MD5.Create();
+        private readonly SHA256 _sha256 = SHA256.Create();
+        private readonly MD5 _md5 = MD5.Create();
 
         public Md5VsSha256()
         {
-            data = new byte[N];
-            new Random(42).NextBytes(data);
+            _data = new byte[N];
+            new Random(42).NextBytes(_data);
         }
 
-        public byte[] Sha256() => sha256.ComputeHash(data);
+        public byte[] Sha256() => _sha256.ComputeHash(_data);
 
-        public byte[] Md5() => md5.ComputeHash(data);
+        public byte[] Md5() => _md5.ComputeHash(_data);
     }
 }
--- PersistenceApp/Benchmarks/Program.cs
+++ PersistenceApp/Benchmarks/Program.cs
@@ -3,6 +3,7 @@ using Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using BenchmarkDotNet.Running;
 
 namespace Benchmarks
 {
@@ -11,37 +12,7 @@ namespace Benchmarks
         static void Main(string[] args)
         {
             //var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
-            //var summary = BenchmarkRunner.Run<Md5VsSha256Benchmarks>();
-
-            //var root = TreeFactory.CreateTree();
-
-            //Console.WriteLine("DFS: ");
-            //var dfs = new DFS();
-            //dfs.Traverse(root);
-
-            //Console.WriteLine("BFS: ");
-            //var bfs = new BFS();
-            //bfs.Traverse(root);
-
-            //var treeTraversals = new TreeTraversals();
-
-            //Console.WriteLine("In order:");
-            //treeTraversals.InOrderTraversal(root);
-
-            //Console.WriteLine("Pre order:");
-            //treeTraversals.PreOrderTraversal(root);
-
-            //Console.WriteLine("Post order:");
-            //treeTraversals.PostOrderTraversal(root);
-
-            IEnumerable<Person> tmp = new List<Person>();
-            Dictionary<int, Person> dict = tmp.ToDictionary(key => key.Id);
-
-            var parenthesisProblem = new ParenthesisProblem();
-            Console.WriteLine(parenthesisProblem.CheckParenthesis("()[]{}"));
-            Console.WriteLine(parenthesisProblem.CheckParenthesis("()[[]{}"));
-
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<Md5VsSha256Benchmarks>();
         }
         
     }
